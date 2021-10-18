91360f3fdcb79676451ed1fbb50b277364b3eef7
Юрий Матыцин <ymatytsin@leads.team>
2021-10-18 13:12:09 +0300

    added 7 task

--- dz-08-01.py
+++ dz-08-01.py
@@ -1,7 +1,7 @@
 
 
-########################################################
-# 3
+# ########################################################
+# # 3
 def sum_digit(n):
     if n > 0:
         return n % 10 + sum_digit(n // 10)
@@ -9,18 +9,18 @@ def sum_digit(n):
         return 'Not supported'
     else:
         return 0
-
-print(sum_digit(123))
+n = int(input())
+sum_digit(n)
 
 # тесты
-print('sum_digit(-5) -', 'Not supported' == sum_digit(-5))
-print('sum_digit(0) -', 0 == sum_digit(0))
-print('sum_digit(1) -', 1 == sum_digit(1))
-print('sum_digit(5) -', 5 == sum_digit(5))
-print('sum_digit(11332) -', 10 == sum_digit(11332))
+assert sum_digit(-5) == 'Not supported', "Should be - Not supported"
+assert sum_digit(0) == 0, "Should be - 0"
+assert sum_digit(1) == 1, "Should be - 1"
+assert sum_digit(5) == 5, "Should be - 5"
+assert sum_digit(11332) == 10, "Should be - 10"
 
-########################################################
-# 4
+# ########################################################
+# # 4
 def fibonachi(n):
     if n in (1, 2):
         return 1
@@ -31,35 +31,36 @@ def fibonachi(n):
     else:
         return fibonachi(n-2) + fibonachi(n-1)
 
-print(fibonachi(11))
+n = int(input())
+fibonachi(n)
 
 # тесты
-print('fibonachi(-5) -', 'Not supported' == fibonachi(-5))
-print('fibonachi(0) -', 0 == fibonachi(0))
-print('fibonachi(1) -', 1 == fibonachi(1))
-print('fibonachi(5) -', 5 == fibonachi(5))
-print('fibonachi(11) -', 89 == fibonachi(11))
+assert fibonachi(-5) == 'Not supported', "Should be - Not supported"
+assert fibonachi(0) == 0, "Should be - 0"
+assert fibonachi(1) == 1, "Should be - 1"
+assert fibonachi(5) == 5, "Should be - 5"
+assert fibonachi(11) == 89, "Should be - 89"
 
 
-########################################################
-# 5
+# ########################################################
+# # 5
 def multiply(lst):
     if len(lst) > 1:
         return lst[-1] * multiply(lst[:-1])
     else:
         return lst[0]
 
-print(multiply([1,-2,3]))
+multiply([1,-2,3])
 
 # # тесты
-print('multiply([1,-2,3]) -', -6 == multiply([1,-2,3]))
-print('multiply([0]) -', 0 == multiply([0]))
-print('multiply([1]) -', 1 == multiply([1]))
-print('multiply([1,2,3]) -', 6 == multiply([1,2,3]))
-print('multiply(1,3,0) -', 0 == multiply([1,3,0]))
+assert multiply([1,-2,3]) == -6, "Should be - -6"
+assert multiply([0]) == 0, "Should be - 0"
+assert multiply([1]) == 1, "Should be - 1"
+assert multiply([1,2,3]) == 6, "Should be - 6"
+assert multiply([1,3,0]) == 0, "Should be - 0"
 
-########################################################
-# 6
+# ########################################################
+# # 6
 def is_power(n):
     n = n/2
     if n == 2:
@@ -71,13 +72,29 @@ def is_power(n):
     else:
         return 'NO'
 
-print(is_power(16))
+n = int(input())
+is_power(n)
 
 # тесты
-print('is_power(-5) -', 'Not supported' == is_power(-5))
-print('is_power(0) -', 'NO' == is_power(0))
-print('is_power(1) -', 'NO' == is_power(1))
-print('is_power(5) -', 'NO' == is_power(5))
-print('is_power(11) -', 'NO' == is_power(11))
-print('is_power(8) -', 'YES' == is_power(8))
-print('is_power(16) -', 'YES' == is_power(16))
+assert is_power(-5) == 'Not supported', "Should be Not supported"
+assert is_power(0) == 'NO', "Should be NO"
+assert is_power(1) == 'NO', "Should be NO"
+assert is_power(5) == 'NO', "Should be NO"
+assert is_power(11) == 'NO', "Should be NO"
+assert is_power(8) == 'YES', "Should be YES"
+assert is_power(16) == 'YES', "Should be YES"
+
+########################################################
+# # 7
+def external_sum(a, b):
+    def inner_sum(a,b):
+        return a + b
+    return inner_sum(a, b) + 5
+
+a,b = int(input()), int(input())
+external_sum(a, b)
+
+# тесты
+assert external_sum(50, 50) == 105, "Should be 105"
+assert external_sum(1, 0) == 6, "Should be 6"
+assert external_sum(0, 0) == 5, "Should be 5"
